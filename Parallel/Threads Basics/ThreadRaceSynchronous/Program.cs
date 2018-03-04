5d9ad902cbc3d2255738d62c9ace142995193c0f
bnaya eshet <bnayanet@hotmail.com>
2018-03-04 15:46:26 +0200

    solution

--- Parallel/Threads Basics/ThreadRaceSynchronous/Program.cs
+++ Parallel/Threads Basics/ThreadRaceSynchronous/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace ThreadRaceSynchronous
 {
@@ -8,39 +9,74 @@ namespace ThreadRaceSynchronous
     {
         private const int THREADS = 3;
         private static Racer[] _racers;
+        private static List<Timer> _timers = new List<Timer>();
+
 
         static void Main(string[] args)
         {
-            int finish = 6;// 85;
+            var gate = new object();
+            int finish = 15;// 85;
             _racers = new Racer[THREADS];
             for (int i = 0; i < THREADS; i++)
             {
-                Predicate<Racer> isWin = race => 
+                Predicate<Racer> isWin = race =>
                                         !_racers
                                             .Where(r => r != race)
                                             .Any(r => r.RaceState == State.Win);
                 string title = ((char)('A' + i)).ToString();
-                _racers[i] = new Racer(title, i, finish, isWin);
+                var racer = new Racer(gate, title, i, finish, isWin);
+                _racers[i] = racer;
+                switch (i % 3)
+                {
+                    case 0:
+                        var trd = new Thread(RunRacerAsync);
+                        trd.Start(racer);
+                        break;
+                    case 1:
+                        ThreadPool.QueueUserWorkItem(RunRacerAsync, racer);
+                        break;
+                    case 2:
+                        var tmr = new Timer(s =>
+                        {
+                            if (racer.RaceState == State.Running)
+                                racer.Advance();
+                        }, racer, 1, 500);
+                        _timers.Add(tmr);
+                        break;
+                    default:
+                        break;
+                }
 
             }
 
             // should be on:
             //  - A = Thread
             //  - B = ThreadPool
-            
-            
+
+
             //  - C = Timer (bonus)
 
-            int step = 0;
-            while (true)
-            {
-                var racer = _racers[step++ % _racers.Length];
-                racer.Advance();
-            }
+            //int step = 0;
+            //while (true)
+            //{
+            //    var racer = _racers[step++ % _racers.Length];
+            //    racer.Advance();
+            //}
 
             // single color per racer
             // only one win
 
+            Console.ReadKey();
+
+        }
+
+        private static void RunRacerAsync(object r)
+        {
+            var racer = (Racer)r;
+            while (racer.RaceState == State.Running)
+            {
+                racer.Advance();
+            }
         }
 
     }
--- Parallel/Threads Basics/ThreadRaceSynchronous/Racer.cs
+++ Parallel/Threads Basics/ThreadRaceSynchronous/Racer.cs
@@ -8,12 +8,15 @@ namespace ThreadRaceSynchronous
     public class Racer
     {
         private readonly Predicate<Racer> _isWinPredicate;
+        private readonly object _gate;
         public Racer(
+            object gate,
             string title,
             int offsetY,
             int finishOffset,
             Predicate<Racer> isWin)
         {
+            _gate = gate;
             Console.SetCursorPosition(0, offsetY);
             Console.WriteLine(title);
 
@@ -49,25 +52,33 @@ namespace ThreadRaceSynchronous
         {
             if (RaceState != State.Running)
                 return;
-            Console.ForegroundColor = _color;
             OffsetX++;
             for (int i = 0; i < 5; i++)
             {
-                Console.SetCursorPosition(OffsetX, OffsetY);
-                switch (i % 2)
+                lock (_gate)
                 {
-                    case 0:
-                        Console.Write("->");
-                        break;
-                    case 1:
-                        Console.Write(" >");
-                        break;
+                    Console.ForegroundColor = _color;
+                    Console.SetCursorPosition(OffsetX, OffsetY);
+                    switch (i % 2)
+                    {
+                        case 0:
+                            Console.Write("->");
+                            break;
+                        case 1:
+                            Console.Write(" >");
+                            break;
+                    }
+                    Console.ResetColor();
                 }
                 Thread.Sleep(50);
             }
-            Console.SetCursorPosition(OffsetX, OffsetY);
-            Console.Write("->");
-            Console.ResetColor();
+            lock (_gate)
+            {
+                Console.ForegroundColor = _color;
+                Console.SetCursorPosition(OffsetX, OffsetY);
+                Console.Write("->");
+                Console.ResetColor();
+            }
 
             if (OffsetX >= FinishOffset + 3)
                 Complete();
@@ -75,31 +86,39 @@ namespace ThreadRaceSynchronous
 
         public void Complete()
         {
-            bool win = _isWinPredicate(this);
             OffsetX++;
             for (int i = 0; i < 5; i++)
             {
-                Console.SetCursorPosition(OffsetX, OffsetY);
-                switch (i % 2)
+                lock (_gate)
                 {
-                    case 0:
-                        Console.Write("=>");
-                        break;
-                    case 1:
-                        Console.Write(" >");
-                        break;
+                    Console.ForegroundColor = _color;
+                    Console.SetCursorPosition(OffsetX, OffsetY);
+                    switch (i % 2)
+                    {
+                        case 0:
+                            Console.Write("=>");
+                            break;
+                        case 1:
+                            Console.Write(" >");
+                            break;
+                    }
                 }
                 Thread.Sleep(50);
             }
-            Console.SetCursorPosition(OffsetX, OffsetY);
-            Console.Write("=> ");
-            if (win)
+            lock (_gate)
             {
-                Console.Write("*");
-                RaceState = State.Win;
+                Console.ForegroundColor = _color;
+                Console.SetCursorPosition(OffsetX, OffsetY);
+                Console.Write("=> ");
+                bool win = _isWinPredicate(this);
+                if (win)
+                {
+                    Console.Write("*");
+                    RaceState = State.Win;
+                }
+                else
+                    RaceState = State.Lose;
             }
-            else
-                RaceState = State.Lose;
 
             Console.ResetColor();
         }
