e18c1b2fb57b4e4f75ba7aec4f088ed2691b496a
bnaya eshet <bnayanet@hotmail.com>
2018-03-04 08:16:18 +0200

    seed

---
+++ Parallel/Threads Basics/ThreadRaceSynchronous/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+
+namespace ThreadRaceSynchronous
+{
+    class Program
+    {
+        private const int THREADS = 3;
+        private static Racer[] _racers;
+
+        static void Main(string[] args)
+        {
+            int finish = 6;// 85;
+            _racers = new Racer[THREADS];
+            for (int i = 0; i < THREADS; i++)
+            {
+                Predicate<Racer> isWin = race => 
+                                        !_racers
+                                            .Where(r => r != race)
+                                            .Any(r => r.RaceState == State.Win);
+                string title = ((char)('A' + i)).ToString();
+                _racers[i] = new Racer(title, i, finish, isWin);
+
+            }
+
+            // should be on:
+            //  - A = Thread
+            //  - B = ThreadPool
+            
+            
+            //  - C = Timer (bonus)
+
+            int step = 0;
+            while (true)
+            {
+                var racer = _racers[step++ % _racers.Length];
+                racer.Advance();
+            }
+
+            // single color per racer
+            // only one win
+
+        }
+
+    }
+}
---
+++ Parallel/Threads Basics/ThreadRaceSynchronous/Racer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadRaceSynchronous
+{
+    public class Racer
+    {
+        private readonly Predicate<Racer> _isWinPredicate;
+        public Racer(
+            string title,
+            int offsetY,
+            int finishOffset,
+            Predicate<Racer> isWin)
+        {
+            Console.SetCursorPosition(0, offsetY);
+            Console.WriteLine(title);
+
+            Title = title;
+            switch (offsetY % 4)
+            {
+                case 0:
+                    _color = ConsoleColor.White;
+                    break;
+                case 1:
+                    _color = ConsoleColor.Yellow;
+                    break;
+                case 2:
+                    _color = ConsoleColor.Green;
+                    break;
+                case 3:
+                    _color = ConsoleColor.Magenta;
+                    break;
+            }
+            OffsetY = offsetY;
+            FinishOffset = finishOffset;
+            _isWinPredicate = isWin;
+        }
+
+        public string Title { get; }
+        public ConsoleColor _color { get; }
+        public int OffsetX = 3;
+        public int OffsetY { get; }
+        public int FinishOffset { get; }
+        public State RaceState { get; private set; }
+
+        public void Advance()
+        {
+            if (RaceState != State.Running)
+                return;
+            Console.ForegroundColor = _color;
+            OffsetX++;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(OffsetX, OffsetY);
+                switch (i % 2)
+                {
+                    case 0:
+                        Console.Write("->");
+                        break;
+                    case 1:
+                        Console.Write(" >");
+                        break;
+                }
+                Thread.Sleep(50);
+            }
+            Console.SetCursorPosition(OffsetX, OffsetY);
+            Console.Write("->");
+            Console.ResetColor();
+
+            if (OffsetX >= FinishOffset + 3)
+                Complete();
+        }
+
+        public void Complete()
+        {
+            bool win = _isWinPredicate(this);
+            OffsetX++;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(OffsetX, OffsetY);
+                switch (i % 2)
+                {
+                    case 0:
+                        Console.Write("=>");
+                        break;
+                    case 1:
+                        Console.Write(" >");
+                        break;
+                }
+                Thread.Sleep(50);
+            }
+            Console.SetCursorPosition(OffsetX, OffsetY);
+            Console.Write("=> ");
+            if (win)
+            {
+                Console.Write("*");
+                RaceState = State.Win;
+            }
+            else
+                RaceState = State.Lose;
+
+            Console.ResetColor();
+        }
+    }
+}
